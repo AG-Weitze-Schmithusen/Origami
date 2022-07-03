921daa8de4ee434b193dfaba2291eaf3f0f1b311
Alexander Rogovskyy <s8alrogo@stud.uni-saarland.de>
2022-07-03 21:41:13 +0200

    remove duplicate docs from functionality.xml

--- lib/special_origamis.gd
+++ lib/special_origamis.gd
@@ -8,7 +8,7 @@
 #! @Returns An Origami
 #! @Description
 #! This function returns a random origami of degree d. It is usually used for testing.
-#! As it is randomized over its permutations the propability distribution is not guaranteed to be uniform on the orbits.
+#! As it is randomized over its permutations the propability distribution is not guaranteed to be uniform on the orbits.#
 DeclareGlobalFunction("RandomOrigami");
 #! @Arguments n
 #! @Returns A special origami
@@ -16,18 +16,32 @@ DeclareGlobalFunction("RandomOrigami");
 #! This function returns special origamis, so calles Xorigamis. Xorigamis have degree 2d.
 #! The horizontal permutations are trivial and the vertical permutations are of the form:
 #! v=(1,2)(3,4)..(2k-1,2k);
+#! @BeginExampleSession
+#! gap> XOrigami(2);
+#! Origami((1,2,3,4), (1,2)(3,4), 4)
+#! @EndExampleSession
 DeclareGlobalFunction("XOrigami");
+
 #! @Arguments a b d
 #! @Returns A special origami
 #! @Description
 #! The elevetor origami consists of d steps of height b and length a.
 #! Each steps are horizontally connected and the vertical permutations are:
 #! v=(a, a+1,..., a+b)(2a+b,2a+b+1,...2a+2b+1)...(da+(d-1)+b,...,d(a+b),1)
+#! @BeginExampleSession
+#! gap>  Elevator(2,0,3);
+#! Origami((1,2)(3,5)(4,6), (1,3)(2,4)(5,6), 6)
+#! @EndExampleSession
  DeclareGlobalFunction("ElevatorOrigami");
+
  #! @Arguments a b d
  #! @Returns A special origami
  #! @Description
  #! The staircase origami consists of d steps of height b and length a.
  #! Each steps are horizontally connected and the vertical permutations are:
  #! v=(a, a+1,..., a+b+1)(2a+b,2a+b+1,...2a+2b+1)...(da+(d-1)+b,...,d(a+b))
+ #! @BeginExampleSession
+ #! gap> Staircase(2,0,3);
+ #! Origami((1,2)(3,4)(5,6), (2,3)(4,5), 6)
+ #! @EndExampleSession
 DeclareGlobalFunction("StaircaseOrigami");
