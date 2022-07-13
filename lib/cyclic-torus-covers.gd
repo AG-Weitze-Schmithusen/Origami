a33fe05ea19c4011319fc2d74504a1cbbb6e4742
Alexander Rogovskyy <s8alrogo@stud.uni-saarland.de>
2022-07-13 16:38:15 +0200

    small layout change

--- lib/cyclic-torus-covers.gd
+++ lib/cyclic-torus-covers.gd
@@ -48,7 +48,8 @@ DeclareGlobalFunction("GeneralizedCyclicTorusCover");
 #!  $(0,0)$, $(n/2, n/2)$, $(n/2,0)$ or $(0,n/2)$. The coordinates are considered modulo $n$. See <Cite Key="comb" /> for more details.
 #! @BeginExampleSession
 #! gap> CombOrigami(3, 1, 0);
-#! Origami((1,2,3)(4,5,6,13,14,15)(7,8,9)(10,11,12)(16,17,18), (1,4,7)(2,5,8,11,14,17)(3,6,9)(10,13,16)(12,15,18), 18)
+#! Origami((1,2,3)(4,5,6,13,14,15)(7,8,9)(10,11,12)(16,17,18), 
+#! (1,4,7)(2,5,8,11,14,17)(3,6,9)(10,13,16)(12,15,18), 18)
 #! @EndExampleSession
 DeclareGlobalFunction("CombOrigami");
 
