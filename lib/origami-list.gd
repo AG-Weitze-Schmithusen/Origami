31344878f9aa344d1472f60b4027f426d62c7924
Leo Emmerich <leem00001@stud.uni-saarland.de>
2025-07-21 16:05:39 +0200

    write sentences for autodoc

--- lib/origami-list.gd
+++ lib/origami-list.gd
@@ -1,3 +1,20 @@
 DeclareGlobalFunction("AllOrigamisByDegree");
 DeclareGlobalFunction("AllOrigamisInStratum");
+
+#! @Arguments d
+#! @Returns The number of origamis of degree d up to equivalence.
+#! @Description 
+#! This number equals the number of subgroups of index d in free group on 2 generators up to conjugacy.
+#! The backend of this procedure works within that setting.
+#! - There is 1 degree 1 origami and hence 1 equivalence class.
+#! - The equivalence classes of degree 2 origamis can by represented by {(id, id), (id, (1, 2)), ((1, 2), (1, 2))}.
+#! - Of degree 3 origamis by {(id, (1, 2, 3)), ((1, 2), (1, 3)), ((1, 2), (1, 2, 3)), ((1, 2, 3), id), ((1, 2, 3), (1, 2)), ((1, 2, 3), (1, 2, 3)), ((1, 2, 3), (3, 2, 1))}.
+#! @BeginExampleSession
+#! gap> NumberOfOrigamisByDegree(1);
+#! 1
+#! gap> NumberOfOrigamisByDegree(2);
+#! 3
+#! gap> NumberOfOrigamisByDegree(3);
+#! 7
+#! @EndExampleSession
 DeclareGlobalFunction("NumberOfOrigamisByDegree");
