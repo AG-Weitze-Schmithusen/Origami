05fec7b0c859a31635ea747e371a9d353633c585
Sebastian Engelhardt <seen00001@stud.uni-saarland.de>
2026-01-28 15:33:59 +0100

    Adjusted and extended origami-list docs

--- lib/origami-list.gd
+++ lib/origami-list.gd
@@ -1,14 +1,44 @@
-DeclareGlobalFunction("AllOrigamisByDegree");
-DeclareGlobalFunction("AllOrigamisInStratum");
+#! @Chapter Functions for listing Origamis
+#! @ChapterLabel Functions for listing Origamis
+#! This section describes functions that list or count all origamis with certain properties.
+
+#! @Section Listing Origamis
 
 #! @Arguments d
-#! @Returns The number of origamis of degree d up to equivalence.
+#! @Returns a list of origamis
 #! @Description 
-#! This number equals the number of subgroups of index d in free group on 2 generators up to conjugacy.
-#! The backend of this procedure works within that setting.
-#! - There is 1 degree 1 origami and hence 1 equivalence class.
-#! - The equivalence classes of degree 2 origamis can by represented by {(id, id), (id, (1, 2)), ((1, 2), (1, 2))}.
-#! - Of degree 3 origamis by {(id, (1, 2, 3)), ((1, 2), (1, 3)), ((1, 2), (1, 2, 3)), ((1, 2, 3), id), ((1, 2, 3), (1, 2)), ((1, 2, 3), (1, 2, 3)), ((1, 2, 3), (3, 2, 1))}.
+#! This function returns a list of all origamis of degree $d$ up to equivalence.
+#! @BeginExampleSession
+#! gap> AllOrigamisByDegree(2);
+#! [ Origami((), (1,2), 2), Origami((1,2), (), 2), Origami((1,2), (1,2), 2) ]
+#! gap> AllOrigamisByDegree(3);
+#! [ Origami((), (1,2,3), 3), Origami((1,2), (2,3), 3), Origami((1,2), (1,2,3), 3),
+#!   Origami((1,2,3), (), 3), Origami((1,2,3), (2,3), 3),
+#!   Origami((1,2,3), (1,2,3), 3), Origami((1,2,3), (1,3,2), 3) ]
+#! @EndExampleSession
+DeclareGlobalFunction("AllOrigamisByDegree");
+
+#! @Arguments d, strat
+#! @Returns a list of origamis
+#! @Description 
+#! This function returns a list of all origamis of degree $d$ in the given stratum up to equivalence.
+#! @BeginExampleSession
+#! gap> AllOrigamisInStratum(4, [1,1]);
+#! [ Origami((1,2), (1,3)(2,4), 4), Origami((1,2), (1,3,2,4), 4),
+#!   Origami((1,2)(3,4), (2,3), 4), Origami((1,2)(3,4), (2,3,4), 4),
+#!   Origami((1,2)(3,4), (1,2,3,4), 4), Origami((1,2,3), (2,3,4), 4), 
+#!   Origami((1,2,3), (2,4,3), 4), Origami((1,2,3), (1,2)(3,4), 4),
+#!   Origami((1,2,3,4), (2,4), 4), Origami((1,2,3,4), (1,2)(3,4), 4) ]
+#! @EndExampleSession
+DeclareGlobalFunction("AllOrigamisInStratum");
+
+#! @Section Counting Origamis
+
+#! @Arguments d
+#! @Returns a positive integer
+#! @Description 
+#! This function returns the number of origamis of degree $d$ up to equivalence, i.e. the number of subgroups
+#! of index $d$ in the free group on 2 generators up to conjugacy.
 #! @BeginExampleSession
 #! gap> NumberOfOrigamisByDegree(1);
 #! 1
